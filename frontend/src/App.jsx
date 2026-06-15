1b51d1628d914ce58f9cda82d84b9826cc8cdb3d
Bristi Mahata <bristimahata0303@gmail.com>
2026-06-15 21:54:06 +0530

    Build dashboard KPI cards and revenue chart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip
+} from "recharts";
+
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
   const [summary, setSummary] = useState(null);
+  const [trends, setTrends] = useState([]);
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/api/summary")
       .then((res) => setSummary(res.data))
+      .catch((err) => console.error(err));
+
+    axios
+      .get("http://localhost:5000/api/trends")
+      .then((res) => setTrends(res.data))
       .catch((err) => console.error(err));
   }, []);
 
@@ -24,6 +39,7 @@ function App() {
           display: "flex",
           gap: "20px",
           marginTop: "20px",
+          marginBottom: "40px",
         }}
       >
         <div style={{ border: "1px solid gray", padding: "20px" }}>
@@ -41,6 +57,24 @@ function App() {
           <p>{summary.topRegion}</p>
         </div>
       </div>
+
+      <h2>Revenue Trend</h2>
+
+      <LineChart
+        width={900}
+        height={400}
+        data={trends}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis />
+        <Tooltip />
+        <Line
+          type="monotone"
+          dataKey="revenue"
+          stroke="#8884d8"
+        />
+      </LineChart>
     </div>
   );
 }
