a62be8a4762595e30c4e7b8da1e4773ff0ad11e1
nayanmahata6290-dotcom <nayanmahata6290@gmail.com>
2026-06-15 20:32:21 +0530

    Add gitignore and remove node_modules

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const db = new Database("db.sqlite");
 
-app.get("/api/summary", (req, res) => {
+/*app.get("/api/summary", (req, res) => {
   const result = db.prepare(`
     SELECT
       ROUND(SUM(net_revenue_usd),2) as totalRevenue,
@@ -18,6 +18,53 @@ app.get("/api/summary", (req, res) => {
   `).get();
 
   res.json(result);
+});
+*/
+
+app.get("/api/summary", (req, res) => {
+
+  const summary = db.prepare(`
+    SELECT
+      ROUND(SUM(net_revenue_usd),2) as totalRevenue,
+      SUM(units_sold) as totalUnits,
+      ROUND(
+        SUM(gross_profit_usd) * 100.0 /
+        SUM(net_revenue_usd),2
+      ) as margin
+    FROM sales
+  `).get();
+
+  const topRegion = db.prepare(`
+    SELECT region
+    FROM sales
+    GROUP BY region
+    ORDER BY SUM(net_revenue_usd) DESC
+    LIMIT 1
+  `).get();
+
+  const topChannel = db.prepare(`
+    SELECT channel
+    FROM sales
+    GROUP BY channel
+    ORDER BY SUM(net_revenue_usd) DESC
+    LIMIT 1
+  `).get();
+
+  const topProduct = db.prepare(`
+    SELECT product_name
+    FROM sales
+    GROUP BY product_name
+    ORDER BY SUM(net_revenue_usd) DESC
+    LIMIT 1
+  `).get();
+
+  res.json({
+    ...summary,
+    topRegion: topRegion.region,
+    topChannel: topChannel.channel,
+    topProduct: topProduct.product_name
+  });
+
 });
 
 app.listen(5000, () => {
